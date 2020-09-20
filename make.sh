f3ea4a4826846ab5444109aee30ebbca94083d03
mhio <37205532+mhio@users.noreply.github.com>
2020-09-20 16:36:12 +1000

    add helm package and repo buil

---
+++ make.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -ue
+
+rundir=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")
+canonical="$rundir/$(basename -- "$0")"
+
+if [ -n "${1:-}" ]; then
+  cmd=$1
+  shift
+else
+  cmd=build
+fi
+
+cd "$rundir"
+
+###
+
+run_build_repo_index(){
+  helm package gogs/ -u
+  helm repo index .
+}
+
+
+###
+
+run_help(){
+  echo "Commands:"
+  awk '/  ".*"/{ print "  "substr($1,2,length($1)-3) }' make.sh
+}
+set +x
+case $cmd in
+  "build")                  run_build_repo_index "$@";;
+
+  '-h'|'--help'|'h'|'help') run_help;;
+  *)                        $cmd "$@";;
+esac
+
